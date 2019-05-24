92a190bed7315bf9e459f745667a97e254129164
Felipe Divensi <divensifelipe@gmail.com>
2019-05-24 11:32:59 -0300

    adicionando datahora no comeco de cada linha

--- leitor-serial/leitor-serial.py
+++ leitor-serial/leitor-serial.py
@@ -1,25 +1,21 @@
 import serial, time, glob, os
+from datetime import datetime
 
-ser = serial.Serial('/dev/ttyACM0', 9600) # porta no Arduino
-#ser = serial.Serial('/dev/ttyS4', 9600) # teste no WSL
+#ser = serial.Serial('/dev/ttyACM0', 9600) # porta no Arduino
+ser = serial.Serial('/dev/ttyS4', 9600) # teste no WSL
 
 while 1:
-    serial_line = ser.readline()
+    serial_line = ser.readline().decode("utf-8")
 
-    print(serial_line.decode("utf-8")) # If using Python 2.x use: print serial_line
+    print_line = "\"" + datetime.now().isoformat() + "\"," + serial_line
+
+    print(print_line) # If using Python 2.x use: print serial_line
     # Do some other work on the data
     
-    #try:
     with open('serial.csv', 'a') as file:
-        file.write(serial_line.decode("utf-8"))
-
-    #except:
-    #print('Nao foi possivel abrir o arquivo para escrita')
-
+        file.write(print_line)
 
     time.sleep(1) # sleep 5 minutes
-
-    # Loop restarts once the sleep is finished
 
 ser.close() # Only executes once the loop exits
 
